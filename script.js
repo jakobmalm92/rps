9acd5d039c1a734d5798c6e41057288edb5d36c3
jakob.malm <malm92_rhotmail.com>
2023-12-06 15:03:10 +0100

    fortsätter med kommentarer i scripten

--- script.js
+++ script.js
@@ -18,91 +18,83 @@ const storedPlayerName = localStorage.getItem("playerName"); //Hämtar tidigare 
 const storedUserScore = localStorage.getItem("userScore"); //Hämtar tidigare sparad poäng
 const storedComputerScore = localStorage.getItem("computerScore");
 
-
-playerName = storedPlayerName || ""; //Finns det inget sparat används en tom sträng
-userScore = parseInt(storedUserScore) || 0;
+playerName = storedPlayerName || ""; //Värdet för spelarnamnet som hämtas från localStorage
+userScore = parseInt(storedUserScore) || 0; //Innehåller värdet för användarens poäng
 computerScore = parseInt(storedComputerScore) || 0;
 
-updateUI();
-
 homeBtn.addEventListener("click", function (event) {
-    startPage.style.display = "block";
-    gamePage.style.display = "none";
+  startPage.style.display = "block"; //Visar startsidan
+  gamePage.style.display = "none"; //Döljer spelarsidan
 
-    
-    userScore = 0;
-    computerScore = 0;
+  userScore = 0; //Återställer användarens poäng
+  computerScore = 0; //Återställer datorns poäng
 
-    updateUI();
+  localStorage.removeItem("playerName"); //Ta bort spelarnamn från localStorage
+  localStorage.removeItem("userScore");//Ta bort användarens poäng från localStorage
+  localStorage.removeItem("computerScore");//Ta bort datorns poäng från localStorage
 
-    localStorage.removeItem("playerName");
-    localStorage.removeItem("userScore");
-    localStorage.removeItem("computerScore");
-
-    nameForm.reset();
+  nameForm.reset();
 });
 
 startGameBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    playerName = document.getElementById("name").value;
+  event.preventDefault();
+  playerName = document.getElementById("name").value;//Hämtar värdet i input och tilldelar det till playerName
 
-    if (!playerName) {
-        alert("Skriv in ditt namn");
-        return;
-    }
+  if (!playerName) { //Kontrollerar om spelarnamnet är tomt
+    alert("Skriv in ditt namn");
+    return;
+  }
 
-    localStorage.setItem("playerName", playerName);
+  localStorage.setItem("playerName", playerName); //Sparar spelarnamnet i localStorage för att komma ihåg det mellan sidladdningar
 
-    playerNameDisplay.textContent = `Player: ${playerName}`;
-    userLabel.textContent = playerName;
-    startPage.style.display = "none";
-    gamePage.style.display = "block";
+  playerNameDisplay.textContent = `Player: ${playerName}`; //Uppdaterar med det valda användarnamnet
+  userLabel.textContent = playerName; //Uppdaterar med det valda användarnamnet
+  startPage.style.display = "none"; //Döljer startsidan
+  gamePage.style.display = "block"; //Visar spelarsidan
 });
 
-choices.forEach(choice => {
-    choice.addEventListener("click", function () {
-        const playerChoice = choice.id;
-        const computerChoice = getComputerChoice();
-        const result = winner(playerChoice, computerChoice);
-        resultDisplay.textContent = result;
+choices.forEach((choice) => {
+  choice.addEventListener("click", function () {
+    const playerChoice = choice.id;
+    const computerChoice = getComputerChoice();
+    const result = winner(playerChoice, computerChoice);
+    resultDisplay.textContent = result;
 
-        if (result.includes("vann")) {
-            userScore++;
-        } else if (result.includes("förlorade")) {
-            computerScore++;
-        }
+    if (result.includes("vann")) {
+      userScore++;
+    } else if (result.includes("förlorade")) {
+      computerScore++;
+    }
 
-        
-        localStorage.setItem("userScore", userScore);
-        localStorage.setItem("computerScore", computerScore);
+    localStorage.setItem("userScore", userScore);
+    localStorage.setItem("computerScore", computerScore);
 
-        updateUI();
-    });
+    updateUI();
+  });
 });
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
 }
 
 function winner(player, computer) {
-    if (player === computer) {
-        return "Oavgjort!";
-    } else if (
-        (player === "rock" && computer === "scissors") ||
-        (player === "paper" && computer === "rock") ||
-        (player === "scissors" && computer === "paper")
-    ) {
-        return "Du vann!";
-    } else {
-        return "Du förlorade!";
-    }
+  if (player === computer) {
+    return "Oavgjort!";
+  } else if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    return "Du vann!";
+  } else {
+    return "Du förlorade!";
+  }
 }
 
 function updateUI() {
-    userScoreSpan.textContent = userScore;
-    computerScoreSpan.textContent = computerScore;
+  userScoreSpan.textContent = userScore;
+  computerScoreSpan.textContent = computerScore;
 }
-
-
+updateUI();
